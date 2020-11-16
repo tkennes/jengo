b1376a2b4585bdeb818853b21cf3c3d81448ece5
Tom Kennes <tomkennes@gskyworkz.nl>
2020-11-16 15:49:18 +0100

    update

--- common/endpoint_test.go
+++ common/endpoint_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 )
 
+var _ = func() bool {
+	testing.Init()
+	return true
+}()
+
 func TestGetEndpoint(t *testing.T) {
 
 	expectation := "/api/json?tree=jobs[name,color,buildable,url,description]"
